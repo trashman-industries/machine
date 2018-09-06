fc6ce69984f2e72c57de09ef082b329abac64fd5
Kia Rahimian <kia@tintmail.com>
2018-09-06 09:03:21 -0400

    feat: add fantasypros projections

--- config.js
+++ config.js
@@ -1,3 +1,6 @@
+const moment = require('moment')
+
 module.exports = {
+  week_one: moment('2018-08-28'),
   ua: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
 }
