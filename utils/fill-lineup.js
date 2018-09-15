4f26232581202c30ccc8db4c7318ebb2713bdbfc
Kia Rahimian <kia@tintmail.com>
2018-09-15 13:13:27 -0400

    fix: fill lineup total points

--- utils/fill-lineup.js
+++ utils/fill-lineup.js
@@ -2,31 +2,49 @@ const filterByPositions = require('./filter-by-positions')
 
 const fillLineup = (lineup, available) => {
   if (!lineup.qb.player_name) {
-    lineup.qb = filterByPositions(['qb'], available)[0]
+    const p = filterByPositions(['qb'], available)[0]
+    lineup.qb = p
+	lineup.total += p.fantasy_points
   }
   if (!lineup.rb1.player_name) {
-    lineup.rb1 = filterByPositions(['rb'], available)[0]
+    const p = filterByPositions(['rb'], available)[0]
+    lineup.rb1 = p
+	lineup.total += p.fantasy_points
   }
   if (!lineup.rb2.player_name) {
-    lineup.rb2 = filterByPositions(['rb'], available)[0]
+    const p = filterByPositions(['rb'], available)[0]
+    lineup.rb2 = p
+	lineup.total += p.fantasy_points
   }
   if (!lineup.wr1.player_name) {
-    lineup.wr1 = filterByPositions(['wr'], available)[0]
+    const p = filterByPositions(['wr'], available)[0]
+    lineup.wr1 = p
+	lineup.total += p.fantasy_points
   }
   if (!lineup.wr2.player_name) {
-    lineup.wr2 = filterByPositions(['wr'], available)[0]
+    const p = filterByPositions(['wr'], available)[0]
+    lineup.wr2 = p
+	lineup.total += p.fantasy_points
   }
   if (!lineup.te.player_name) {
-    lineup.te = filterByPositions(['te'], available)[0]
+    const p = filterByPositions(['te'], available)[0]
+    lineup.te = p
+    lineup.total += p.fantasy_points
   }
   if (!lineup.k.player_name) {
-    lineup.k = filterByPositions(['k'], available)[0]
+    const p = filterByPositions(['k'], available)[0]
+    lineup.k = p
+    lineup.total += p.fantasy_points
   }
   if (!lineup.flex.player_name) {
-    lineup.flex = filterByPositions(['rb','wr'], available)[0]
+    const p = filterByPositions(['rb','wr'], available)[0]
+    lineup.flex = p
+    lineup.total += p.fantasy_points
   }
   if (!lineup.dst.player_name) {
-    lineup.dst = filterByPositions(['dst'], available)[0]
+    const p = filterByPositions(['dst'], available)[0]
+    lineup.dst = p
+    lineup.total += p.fantasy_points
   }
 
   return lineup
